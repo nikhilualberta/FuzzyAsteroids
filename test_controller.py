943b331abbc9c88e9fae6739c5741fedefc44a2a
nikhilualberta <nnayyar1@ualberta.ca>
2023-11-23 18:47:37 -0700

    reverted to version 1.3.6

--- test_controller.py
+++ test_controller.py
@@ -2,31 +2,23 @@
 # Copyright © 2022 Thales. All Rights Reserved.
 # NOTICE: This file is subject to the license agreement defined in file 'LICENSE', which is part of
 # this source code package.
-
-from kesslergame import KesslerController
+from kesslergame import KesslerController # In Eclipse, the name of the library is kesslergame, not src.kesslergame
 from typing import Dict, Tuple
-
+import skfuzzy as fuzz
 
 class TestController(KesslerController):
     def __init__(self):
         self.eval_frames = 0
-
-    def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool, bool]:
+    def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool]:
         """
         Method processed each time step by this controller.
         """
-
         thrust = 0
         turn_rate = 90
         fire = True
-        drop_mine = False
-        dropped_mine = False
-        if game_state["time"] >= 1.0 and not len(game_state["mines"]) > 0:
-            drop_mine = True
         self.eval_frames +=1
-
-        return thrust, turn_rate, fire, drop_mine
-
+        return thrust, turn_rate, fire
+    
     @property
     def name(self) -> str:
         return "Test Controller"
