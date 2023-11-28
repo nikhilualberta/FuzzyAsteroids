a21bf8ee28c0cf2769ba58a800d7a07089f3cf63
koss756 <lkosedy24@gmail.com>
2023-11-27 17:41:39 -0700

    threat controller

--- scenario_test.py
+++ scenario_test.py
@@ -14,7 +14,7 @@ my_test_scenario = Scenario(name='Test Scenario',
                             {'position': (400, 400), 'angle': 90, 'lives': 3, 'team': 1},
                             {'position': (600, 400), 'angle': 90, 'lives': 3, 'team': 2},
                             ],
-                            map_size=(1000, 800),
+                            map_size=(800, 800),
                             time_limit=60,
                             ammo_limit_multiplier=0,
                             stop_if_no_ammo=False)
@@ -23,7 +23,7 @@ game_settings = {'perf_tracker': True,
                 'graphics_type': GraphicsType.Tkinter,
                 'realtime_multiplier': 1,
                 'graphics_obj': None}
-
+6
 game = KesslerGame(settings=game_settings) # Use this to visualize the game scenario
 # game = TrainerEnvironment(settings=game_settings) # Use this for max-speed, no-graphics simulation
 pre = time.perf_counter()
